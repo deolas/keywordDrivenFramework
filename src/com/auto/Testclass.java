c8ba96b4b52209ee7e3aa93a0f7d6435c7c73469
Shahnawaz <mohd.786.shahnawaz@gmail.com>
2020-02-13 16:59:12 +0530

    Initial KeywordDriven Changes

---
+++ src/com/auto/Testclass.java
@@ -0,0 +1,12 @@
+package com.auto;
+
+public class Testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Git is tested");
+
+	}
+
+}
